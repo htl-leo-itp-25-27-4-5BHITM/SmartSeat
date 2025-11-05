0e05411b7544cdc6e6dbe02c1462b492dc41eb4b
Stefan Meinhart <s.meinhart@htblaleonding.onmicrosoft.com>
2025-11-05 11:34:32 +0100

    added functions to rep

---
+++ project/java-quarkus/src/main/java/at/htl/model/Seat.java
@@ -0,0 +1,48 @@
+package at.htl.model;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CurrentTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+
+    private Long id;
+    private String location;
+    private SeatStatus status;
+    //@CurrentTimestamp
+    private LocalDateTime timeStamp;
+
+    public Seat (String location, SeatStatus status, LocalDateTime timeStamp) {
+        setLocation(location);
+        setStatus(status);
+        setTimeStamp(timeStamp);
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public SeatStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(SeatStatus status) {
+        this.status = status;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
