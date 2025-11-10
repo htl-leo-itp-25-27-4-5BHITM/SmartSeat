4f34a4cdcf76488ee2154a982207b733a5176ca6
Stefan Meinhart <s.meinhart@htblaleonding.onmicrosoft.com>
2025-11-10 11:18:33 +0100

    finished repo to update the seats

--- project/quarkus-java/src/main/java/at/htl/repository/Repository.java
+++ project/quarkus-java/src/main/java/at/htl/repository/Repository.java
@@ -2,22 +2,47 @@ package at.htl.repository;
 
 import at.htl.model.Seat;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 import java.util.List;
 
 
 @ApplicationScoped
 public class Repository {
+    @Inject
+    EntityManager en;
+
+    @Transactional
     public boolean addSeat(Seat seat) {
-        return  false;
+        en.persist(seat);
+        return  en.find(Seat.class,seat) != null;
     }
     public boolean updateSeat (Seat seat) {
+        if (en.find(Seat.class, seat) != null) {
+            en.merge(seat);
+            return true;
+        }
         return false;
     }
     public List<Seat> getAll () {
-        return null;
+        var query = en.createQuery("select c from seatUse c");
+
+        return query.getResultList();
     }
     public Seat getSeatById (Long id) {
+        return en.find(Seat.class,id);
+    }
+
+    public List<Seat> getFilteredSeats (String filter) {
+        var query = en.createQuery("select c from seatUse c where lower(name) like lower('%'||:filter||'%')" +
+                " or lower(location) like lower('%'||:filter||'%')");
+        query.setParameter("filter",filter);
+
+        return query.getResultList();
+    }
+    public List<Seat> getLastUsedSeats() {
         return null;
     }
 
