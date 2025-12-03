bdcc6d3fbca65b0b8c59dfeb0417c0d52291034e
Daniel31507 <d.riemer@htblaleonding.onmicrosoft.com>
2025-12-03 08:36:22 +0100

    final

--- project/quarkus-java/src/main/resources/import.sql
+++ project/quarkus-java/src/main/resources/import.sql
@@ -1,10 +1,12 @@
-INSERT INTO SEATLOCATION (WING, FLOOR) VALUES ('Left', '1OG' ),
-                                              ('Right', '1OG' ),
-                                              ('Left', '2OG' ),
-                                              ('Right', '2OG' );
+INSERT INTO SEATLOCATION (WING, FLOOR)
+VALUES ('Left', '1OG'),
+       ('Right', '1OG'),
+       ('Left', '2OG'),
+       ('Right', '2OG');
 
-INSERT INTO SEAT(Name,STATUS,LOCATION_ID) VALUES ( 'Koje 1', true,1  ),
-                                                 ( 'Koje 2', true,2  ),
-                                                 ( 'Koje 3', true,2  ),
-                                                 ( 'Koje 4', false,3  ),
-                                                 ( 'Koje 5', false,4  );
+INSERT INTO SEAT(Name, STATUS, LOCATION_ID)
+VALUES ('Koje 1', true, 1),
+       ('Koje 2', true, 2),
+       ('Koje 3', true, 2),
+       ('Koje 4', false, 3),
+       ('Koje 5', false, 4);
