6c127472427265c577efca18c97f6a04cb7746cf
Daniel31507 <d.riemer@htblaleonding.onmicrosoft.com>
2025-12-03 10:44:34 +0100

    added inserts

--- project/quarkus-java/src/main/resources/import.sql
+++ project/quarkus-java/src/main/resources/import.sql
@@ -10,3 +10,14 @@ VALUES ('Koje 1', true, 1),
        ('Koje 3', true, 2),
        ('Koje 4', false, 3),
        ('Koje 5', false, 4);
+
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('8:50', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('9:45', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('10:50', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('11:45', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('12:40', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('13:35', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('14:30', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('15:25', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('16:20', 'HH24:mi'));
+INSERT INTO ENDTIMES (ENDTIME) VALUES (to_char('17:15', 'HH24:mi'));
