feacffe16e2dc223edb0d39c093d6812f07f5bb5
NinoTesar <n.tesar@HTBLALeonding.onmicrosoft.com>
2026-01-14 11:36:07 +0100

    Database update and Websocket broadcast

--- project/quarkus-java/src/main/java/at/htl/repository/SensorService.java
+++ project/quarkus-java/src/main/java/at/htl/repository/SensorService.java
@@ -1,10 +1,13 @@
 package at.htl.repository;
 
+import at.htl.model.Seat;
 import at.htl.model.SensorMessage;
+import at.htl.sockets.SeatWebSocket;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 import java.io.IOException;
@@ -15,15 +18,28 @@ public class SensorService {
     @Inject
     EntityManager em;
 
+    @Inject
+    SeatWebSocket seatWebSocket;
+
+    @Transactional
     @Incoming("pico-data")
     public void handleIncoming(byte[] raw) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
 
         SensorMessage sensorMessage = objectMapper.readValue(raw, SensorMessage.class);
 
-        System.out.println("Geht es!?");
+        System.out.println("<===Übertragung===>");
         System.out.println(sensorMessage.getName());
         System.out.println(sensorMessage.getStatus());
 
+        var query = em.createQuery("""
+            update Seat s set status = :status where name = :name
+        """);
+        query.setParameter("status", sensorMessage.getStatus());
+        query.setParameter("name", sensorMessage.getName());
+
+        query.executeUpdate();
+
+        seatWebSocket.broadcastSeatUpdate();
     }
 }
