387d90988644edf2cee6c1cb8d6768214b017a0a
Stefan Meinhart <s.meinhart@HTBLALeonding.onmicrosoft.com>
2026-06-10 08:32:20 +0200

    rebuild the WIFI config to .env file

--- project/C_Pi_Movement/C_PI/WLANConf.h
+++ project/C_Pi_Movement/C_PI/WLANConf.h
@@ -1,22 +1,19 @@
 #ifndef WIFI_NAME
-#define WIFI_NAME "smartseat_24Ghz" 
+#define WIFI_NAME std::getenv("WIFI_NAME") 
 #endif
 
 #ifndef WIFI_PASSWORDV
-#define WIFI_PASSWORDV "smartseat123" 
+#define WIFI_PASSWORDV std::getenv("WIFI_PASSWORDV") 
 #endif
 
-// #ifndef MQTT_SERVERV
-// #define MQTT_SERVERV "192.168.188.228" 
-// #endif
 #ifndef MQTT_SERVERV
-#define MQTT_SERVERV "10.191.112.90" 
+#define MQTT_SERVERV std::getenv("MQTT_SERVERV") 
 #endif
 
 #ifndef MQTT_USERNAMEV
-#define MQTT_USERNAMEV "student"
+#define MQTT_USERNAMEV std::getenv("MQTT_USERNAMEV")
 #endif
 
 #ifndef MQTT_PASSWORDV
-#define MQTT_PASSWORDV "passme"
+#define MQTT_PASSWORDV std::getenv("MQTT_PASSWORDV")
 #endif // !MQTT_PASSWORDV
